817fdc6fc561a13c6ad440a910ecb43a68027dbe
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-10-06 15:15:20 +0100

    login function works Co-authored-by: Aishah <aishah92@gmail.com>

--- models/userModels.js
+++ models/userModels.js
@@ -3,13 +3,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 function addUser(user) {
-    console.log(user.username,user.password)
   return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(user.password, salt))
       .then(hashedPwd => {
         user.password = hashedPwd;
-        console.log(user.password);
         return db.query(
           'INSERT INTO users(username, password) VALUES($1, $2) RETURNING *', [user.username, user.password]
         ).then(result => result.rows[0]);
