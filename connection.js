3a10c549be08b880e16f692ab529510c0bd115a6
ephie oyedoh <ephieo@rocketmail.com>
2020-10-07 11:23:42 +0100

    writing signup tests

--- connection.js
+++ connection.js
@@ -3,17 +3,18 @@
 //npm install pg which is the node-postgres library for connecting and querying a postgreSQL database.
 const pg = require('pg');
 const dotenv = require('dotenv');
-const databaseUrl = process.env.DATABASE_URL;
+let databaseurl = process.env.DATABASE_URL;
 
 dotenv.config();
 
 const options = {
-  connectionString: databaseUrl,
-  ssl: { rejectUnauthorized: false }
+  connectionString: databaseurl
+  // ssl: { rejectUnauthorized: false }
+  // ssl: true
 };
 
 if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.TEST_DATABASE_URL;
+  databaseurl = process.env.TEST_DATABASE_URL;
 }
 
 const db = new pg.Pool(options);
--- tests/model.test.js
+++ tests/model.test.js
@@ -1,14 +1,43 @@
 const test = require('tape');
 const supertest = require('supertest');
 require('dotenv').config;
+const build = require('../build');
+const db = require('../connection');
+const { addUser, findUser } = require('./../models/userModels');
 
 /* All model tests */
-
+//finduser
 //test signup- has user been added to db table?
-
+test('Signing up a new user', t => {
+  build().then(() => {
+    const data = {
+      username: 'zenny',
+      password: 'zen2020',
+      id: 4
+    };
+    addUser(data)
+      .then(data => findUser(data.id))
+      .then(user => {
+        username = user.username;
+        console.log('username :', username);
+        t.equal(username, 'zenny');
+        t.end();
+      })
+      .catch(error => {
+        t.error(error);
+        t.end();
+      });
+  });
+});
 //login- will you recieve access token on login with correct username and password?
 
 //authenticate- do you have access to some route that is logged-in user only?
 //are you able to access a route only for authorised users?
 
 //addWork- has the work entry been added to the db?
+
+test('Close DB pool (not a real test)', t => {
+  // otherwise tests will pause for 10s in the terminal
+  db.end();
+  t.end();
+});
